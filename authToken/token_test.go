86f64caef10808c473c1f2bfac58e102da616bab
swxctx <swxctx@gmail.com>
2020-06-25 13:58:07 +0800

    add auth token test

---
+++ authToken/token_test.go
@@ -0,0 +1,18 @@
+package authToken
+
+import (
+	"testing"
+)
+
+func TestAuthToken(t *testing.T) {
+	// gen
+	token := Create(100)
+	t.Logf("Token-> %#v", token)
+
+	// parse
+	parseToken, err := Parse(token.String())
+	if err != nil {
+		t.Fatalf("Parse err-> %v", err)
+	}
+	t.Logf("Parse Token-> %#v", parseToken)
+}
